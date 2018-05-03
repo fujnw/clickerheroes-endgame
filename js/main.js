8db20fd92ab6d28f0f86f17b1eafcece9b2d694f
fuws <36806183+fuws@users.noreply.github.com>
2018-05-02 22:02:27 -0400

    Root2 (#5)
    * Root2 mod support (up to Wepwawet)
    
    * Root2 interface and data updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+var MAX_ZONE = 2**31 - 1;
 var GOLD_SCALE = 1.15;
 var HP_SCALE;   // 2xn array. First row is zones. Second row is hpscales
+var HEROES;
+var ROOT2 = false;
+var ANCIENT_SOULS;
 
 function prepareHPScale() {
-    HP_SCALE = [[1, 140], [1.55, 1.145]];
-    for (i=1; i<=400; i++) {
-        HP_SCALE[0].push(500 * i);
-        HP_SCALE[1].push(1.145 + 0.001 * i);
+    if (!ROOT2) {
+        HP_SCALE = [[1, 140], [1.55, 1.145]];
+        for (i=1; i<=400; i++) {
+            HP_SCALE[0].push(500 * i);
+            HP_SCALE[1].push(1.145 + 0.001 * i);
+        }
+    } else {
+        var bps = root2_customized_bps(ANCIENT_SOULS);
+        HP_SCALE = [[1], [1.55]];
+        for (i=0; i<bps.length; i++) {
+            HP_SCALE[0].push(bps[i][3]);
+            HP_SCALE[1].push(bps[i][2]);
+        }
     }
+    
 }
 
 var HERO_TABLE_COLUMNS = {
@@ -22,13 +36,14 @@ function getHeroAttr(hnum, attr) {
     return HEROES[hnum][HERO_TABLE_COLUMNS[attr]];
 }
 
-function calculateProgression() {    
-    var as = parseFloat($("#inputAS").val());
+function calculateProgression() {
+    ANCIENT_SOULS = parseFloat($("#inputAS").val());
     var lghs = parseFloat($("#inputLgHS").val());
     
     var errMsg = "";
-    if (isNaN(as) || isNaN(lghs)) {
-        errMsg = "Please enter inputs";
+    if (isNaN(ANCIENT_SOULS) || isNaN(lghs) ||
+       $("#gameRoot2").prop("checked") == $("#gameVanilla").prop("checked")) {
+        errMsg = "Please enter all inputs";
     } else if (lghs < 100) {
         errMsg = "Requires lgHS >= 100";
     }
@@ -41,9 +56,13 @@ function calculateProgression() {
         $("#inputWarning").html("");
     }
     
+    ROOT2 = $("#gameRoot2").prop("checked");
+    HEROES = ROOT2 ? HEROES_ROOT2 : HEROES_VANILLA;
+    
     prepareHPScale();
 
-    var tp = 0.25 - 0.23 * Math.exp(-0.0003 * as);
+    var tp = ROOT2 ? root2_tp(ANCIENT_SOULS) : 
+        0.25 - 0.23 * Math.exp(-0.0003 * ANCIENT_SOULS);
     $("#outputTP").val(tp.toFixed(6));
     
     var data = [];
@@ -75,8 +94,11 @@ function calculateProgression() {
             zone.toFixed(0),
             hlevel.toFixed(0),
             lghsChange.toFixed(2),
-            zoneTL < 10000 ? "<10K" : zoneTL.toFixed(0)
+            ROOT2 ? "" : zoneTL.toFixed(0)
         ])
+        if (zone == MAX_ZONE) { // Stop if encountering infinite ascension
+            break;
+        }
         lghsStart += lghsChange;
         start = hnum;
         startTL = hnumTL;
@@ -95,7 +117,7 @@ function heroReached(lgHS, start=0, active=true) {
     for (; i < HEROES.length; i++) {
         zone = zoneReached(lgHS, i, active);
         gold = zone * Math.log10(GOLD_SCALE) + 1.5 * lgHS + 21;
-        if (i == HEROES.length - 1 || 
+        if (zone == MAX_ZONE || i == HEROES.length - 1 || 
             gold < heroUpgradeBaseCost(i + 1)) {
             break;
         }
@@ -106,12 +128,20 @@ function heroReached(lgHS, start=0, active=true) {
 function zoneReached(lgHS, i, active=true) {
     let R = Math.log10(getHeroAttr(i, "damageScale")) / 
         Math.log10(getHeroAttr(i, "costScale")) / 25;
-    let lgDmgMultPerZone = Math.log10(GOLD_SCALE) * R;
+    let lgDmgMultPerZone = Math.log10(GOLD_SCALE) * R + 
+        ROOT2 * (ANCIENT_SOULS >= 6400) * Math.log10(1.01) / 10;
     let efficiency = getHeroAttr(i, 'dps') - 
         R * (getHeroAttr(i, "lv1cost") + 175 * Math.log10(getHeroAttr(i, "costScale")));
+
+    let startingGold = 21.12 + 1.5 * lgHS;
+    startingGold += ROOT2 * (i >= 48) * 98; // BomberMax global gold boost on Root2
     
-    let RHS = (efficiency + (2.4 + (active * 0.5) + 1.5 * R) * lgHS 
-         + 1.86 - 2 + 21.12 * R);   // Minus 2 to account for boss HP
+    let RHS = efficiency + (2.4 + active * 0.5) * lgHS 
+         + 1.86 - 2 + startingGold * R;   // Minus 2 to account for boss HP    
+    RHS += ROOT2 * (i >= 48) * 43.64;   // Gog global DPS boost on Root2
+    
+    let reqzone = (heroUpgradeBaseCost(i) - startingGold) / 
+        Math.log10(GOLD_SCALE);
     
     let lghp0 = 1;  // lgHP at zone 1
     let nbp = HP_SCALE[0].length;
@@ -123,12 +153,10 @@ function zoneReached(lgHS, i, active=true) {
         let z0 = HP_SCALE[0][j];
         let z1 = HP_SCALE[0][j + 1];
         
-        if(lghp0 - lgDmgMultPerZone * z0 > RHS) {
-            // Cannot advance past the first breakpoint
-            return -1;
-        }
-        if(lghp0 - lgDmgMultPerZone * z0 <= RHS && lghp1 - lgDmgMultPerZone * z1 > RHS) {
-            // In the middle of two breakpoints. Solve linear equation.
+        if(z1 >= reqzone && lghp0 - lgDmgMultPerZone * z0 <= RHS && lghp1 - lgDmgMultPerZone * z1 > RHS) {
+            // In the middle of two breakpoints. 
+            // Also must be approximately above the required zone to get this hero
+            // Solve linear equation.
             let M = 1 / (Math.log10(hpscale) - lgDmgMultPerZone);
             let zone = M * (RHS - lghp0 + z0 * Math.log10(hpscale));
             return zone;
@@ -137,6 +165,9 @@ function zoneReached(lgHS, i, active=true) {
     }
     
     let M = 1 / (Math.log10(HP_SCALE[1][nbp - 1]) - lgDmgMultPerZone);
+    if (M < 0) {    // Infinite ascension (monster scaling < damage increase)
+        return MAX_ZONE;
+    }
     let zone = M * (RHS - lghp0 + Math.log10(HP_SCALE[1][nbp - 1]) * HP_SCALE[0][nbp - 1]);
     return zone;
 }
